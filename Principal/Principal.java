0503c95c6d9764b98e5c80b391b561b571f94810
Edwin Alejandro Sanchez Rojas <easanchez@personalsoft.com.co>
2019-12-26 15:08:53 -0500

    Registro de estudiantes

--- Principal/Principal.java
+++ Principal/Principal.java
@@ -1,7 +1,20 @@
 package Principal;
 
+import modelo.Estudiante;
+import javax.swing.JOptionPane;
+
 public class Principal {
     public static void main(String[] args) {
+
+        Estudiante e = new Estudiante( JOptionPane.showInputDialog("Ingrese su nombre"),
+                JOptionPane.showInputDialog("Ingrese su apellido"),
+                JOptionPane.showInputDialog("Ingrese su codigo"));
+
+        JOptionPane.showMessageDialog(null, "Su nombre es: "+ e.getNombre()+
+                "\n Su apellido es: "+ e.getApellido()+
+                "\n Su codigo es: "+ e.getCodigo());
+
+
 
     }
 }
